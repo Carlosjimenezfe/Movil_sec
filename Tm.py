d1b327fa6210ca76bbb55d2b97df8c9941b8b8c0
Carlos Jimenez <valuu1216@gmail.com>
2021-09-03 16:33:10 -0500

    Numero de pulsaciones

--- Tm.py
+++ Tm.py
@@ -13,3 +13,13 @@ volumen = float (input ('Digite el valor del volumen : '))
 temperatura = float ( input ('Digite el valor de la temperatura : '))
 masa = (presion * volumen) / (0.37 * (temperatura + 460))
 print(f'El valor de la masa es : {masa} ')
+
+"""
+2. Calcular el número de pulsaciones que una persona debe tener por
+cada 10 segundos de ejercicio, si la fórmula es:
+Num. Pulsaciones = (200 – edad) /10.
+"""
+
+edad = int (input ('Digite la edad : '))
+numeroPulsaciones = (200 - edad)/10
+print(f'El numero de pulsacion son : {numeroPulsaciones}')
